7195b62cb2b3a1430e827e34e97d5cd84c7ab7bf
markyjf <57089288+markyjf@users.noreply.github.com>
2019-11-02 11:07:42 +0800

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,1 +1,10 @@
-aaaaa
+void reverse(Linklist)
+{LNode *p;
+        p=H->next;
+        H->next=NULL;
+        while(p)
+        {q=p;   p=p->next;
+                q->next=H->next;
+                H->next=q;
+                }
+} 
